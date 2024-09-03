565b84f601d4d50ee683a8f692c3ae0cce385fdb
Nildiert Jimenez <njimenez@buk.co>
2024-09-02 20:06:09 -0500

    feat: Added virtualenv in entrypoint

--- install_scrapper.sh
+++ install_scrapper.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# Define variables
 REPO_URL="https://github.com/nildiert/checho.git"
 INSTALL_DIR="$HOME/Documents/checho"
 
-# Create installation directory
 mkdir -p "$INSTALL_DIR"
 
-# Navigate to installation directory
 cd "$INSTALL_DIR"
 
-# Clone the repository
 git clone "$REPO_URL" .
 
-# Install Python dependencies
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
+
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Make sure the main script is executable
 chmod +x main.py
 
 echo "Installation complete. The repository is installed in $INSTALL_DIR."
+echo "To activate the virtual environment, run 'source $INSTALL_DIR/.venv/bin/activate'"
